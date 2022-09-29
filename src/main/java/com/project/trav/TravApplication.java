b212a49be917bdf1780243bf0bd3a7193ab784c7
kreker1207 <baranetckiy@gmail.com>
2022-09-29 10:47:17 +0300

    Initial setup

---
+++ src/main/java/com/project/trav/TravApplication.java
@@ -0,0 +1,13 @@
+package com.project.trav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/trav/TravApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
