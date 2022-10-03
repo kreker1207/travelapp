1578d1caeaddc08863f39567ebaf085459c2bdac
kreker1207 <baranetckiy@gmail.com>
2022-10-03 19:44:48 +0300

    TRAV-2: rename application

--- src/main/java/com/project/trav/TravelApplication.java
+++ src/main/java/com/project/trav/TravelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravApplication {
+public class TravelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TravApplication.class, args);
+		SpringApplication.run(TravelApplication.class, args);
 	}
 
 }
--- src/test/java/com/project/trav/TravelApplicationTests.java
+++ src/test/java/com/project/trav/TravelApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TravApplicationTests {
+class TravelApplicationTests {
 
 	@Test
 	void contextLoads() {
