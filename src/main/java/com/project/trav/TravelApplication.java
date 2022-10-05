2f928fd0605002c2b05cb2059a86414cfa6ab98a
kreker1207 <baranetckiy@gmail.com>
2022-10-05 18:08:52 +0300

    TRAV-7: delete useless annotation

--- src/main/java/com/project/trav/TravelApplication.java
+++ src/main/java/com/project/trav/TravelApplication.java
@@ -2,10 +2,8 @@ package com.project.trav;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = { "com.project.trav.domain.entity" })
 public class TravelApplication {
 
 	public static void main(String[] args) {
